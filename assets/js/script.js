62dd9a217ba7d52e791171aaa146e3ad6b72acc8
Theo Phillips <phillipstheo88@gmail.com>
2022-04-23 14:10:42 -0700

    added local storage into js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,29 @@ $(document).ready(function () {
      var d = moment().format('MMMM Do YYYY, h:mm:ss a');
      span.textContent = d 
    }
- 
+   setInterval(time, 1000);
+
+    $(".saveBtn").on("click", function () {
+     
+        console.log(this);
+        var text = $(this).siblings(".task").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+        window.alert('Your task has been saved!');
+        console.log('The button has been clicked!');
+    })
+    // load data from storage to display correctly
+    $("#hour7 .task").val(localStorage.getItem("hour7"));
+    $("#hour8 .task").val(localStorage.getItem("hour8"));
+    $("#hour9 .task").val(localStorage.getItem("hour9"));
+    $("#hour10 .task").val(localStorage.getItem("hour10"));
+    $("#hour11 .task").val(localStorage.getItem("hour11"));
+    $("#hour12 .task").val(localStorage.getItem("hour12"));
+    $("#hour13 .task").val(localStorage.getItem("hour13"));
+    $("#hour14 .task").val(localStorage.getItem("hour14"));
+    $("#hour15 .task").val(localStorage.getItem("hour15"));
+    $("#hour16 .task").val(localStorage.getItem("hour16"));
+    $("#hour17 .task").val(localStorage.getItem("hour17"));
+    $("#hour18 .task").val(localStorage.getItem("hour18"));
+    $("#hour19 .task").val(localStorage.getItem("hour19"));
